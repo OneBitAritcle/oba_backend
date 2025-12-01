0b7ebce3f0cd67027871a30dcf9f467c8627c52d
Byunjihun <vkvlditm00@naver.com>
2025-12-02 00:17:09 +0900

    ON-79 GptMongoRepository 수정 - Mongo 쿼리 및 구조 개선

--- server/src/main/java/oba/backend/server/repository/mongo/GptMongoRepository.java
+++ server/src/main/java/oba/backend/server/repository/mongo/GptMongoRepository.java
@@ -1,10 +1,15 @@
 package oba.backend.server.repository.mongo;
 
 import oba.backend.server.entity.mongo.GptDocument;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GptMongoRepository extends MongoRepository<GptDocument, String> {
+
     Optional<GptDocument> findByArticleId(Long articleId);
+
+    List<GptDocument> findByOrderByServingDateDesc(Pageable pageable);
 }
