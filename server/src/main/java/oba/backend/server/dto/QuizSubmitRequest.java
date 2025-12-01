3256bea9dbde9edb3a91a363553711744f61084a
Byunjihun <vkvlditm00@naver.com>
2025-12-02 00:15:33 +0900

    ON-79 QuizSubmitRequest 추가 - 퀴즈 제출 DTO 생성

--- server/src/main/java/oba/backend/server/dto/QuizSubmitRequest.java
+++ server/src/main/java/oba/backend/server/dto/QuizSubmitRequest.java
@@ -1,4 +1,10 @@
 package oba.backend.server.dto;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class QuizSubmitRequest {
+    private Long articleId;
+    private List<Boolean> answers;     // quiz1~quiz5 순서
 }
