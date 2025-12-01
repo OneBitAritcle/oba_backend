afeefb4078cac27732839a71a069c2f7bd87dfbb
Byunjihun <vkvlditm00@naver.com>
2025-12-02 00:15:25 +0900

    ON-79 QuizResultRequest 추가 - 퀴즈 결과 요청 DTO 생성

--- server/src/main/java/oba/backend/server/domain/quiz/QuizResultRequest.java
+++ server/src/main/java/oba/backend/server/domain/quiz/QuizResultRequest.java
@@ -1,4 +1,9 @@
 package oba.backend.server.domain.quiz;
 
+import lombok.Getter;
+
+@Getter
 public class QuizResultRequest {
+    private Long articleId;
+    private boolean[] quizResults;  // true=정답, false=오답
 }
