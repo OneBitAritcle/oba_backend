dc64e9692dc02e58a45b0f209848e729492a6ed6
Byunjihun <vkvlditm00@naver.com>
2025-12-02 00:15:20 +0900

    ON-79 IncorrectArticlesRepository 추가 - 오답 기사 저장 Repository 생성

--- server/src/main/java/oba/backend/server/domain/quiz/IncorrectArticlesRepository.java
+++ server/src/main/java/oba/backend/server/domain/quiz/IncorrectArticlesRepository.java
@@ -1,4 +1,11 @@
 package oba.backend.server.domain.quiz;
 
-public class IncorrectArticlesRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface IncorrectArticlesRepository extends JpaRepository<IncorrectArticles, IncorrectArticlesId> {
+
+    List<IncorrectArticles> findByUserId(Long userId);
+
+    void deleteByUserIdAndArticleId(Long userId, Long articleId);
 }
