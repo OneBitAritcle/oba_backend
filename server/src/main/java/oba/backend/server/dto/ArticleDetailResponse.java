11f1c0f6b4109928be45fabeee136696ed9c6213
Byunjihun <vkvlditm00@naver.com>
2025-12-02 00:16:55 +0900

    ON-79 ArticleDetailResponse 수정 - DTO 필드 및 응답 구조 업데이트

--- server/src/main/java/oba/backend/server/dto/ArticleDetailResponse.java
+++ server/src/main/java/oba/backend/server/dto/ArticleDetailResponse.java
@@ -3,7 +3,6 @@ package oba.backend.server.dto;
 import lombok.Builder;
 import lombok.Data;
 import oba.backend.server.entity.mongo.GptDocument;
-import oba.backend.server.entity.mysql.Article;
 
 import java.util.List;
 
@@ -11,32 +10,15 @@ import java.util.List;
 @Builder
 public class ArticleDetailResponse {
 
-    private Long id;
-    private List<String> category;
+    private Long articleId;
     private String title;
-    private String date;
-    private String url;
+    private String publishTime;
+    private String servingDate;
 
     private Object content;
     private Object subtitle;
 
     private String summary;
-    private List<?> keywords;
-    private List<?> quizzes;
-
-    public static ArticleDetailResponse of(Article a, List<String> c, GptDocument d) {
-
-        return ArticleDetailResponse.builder()
-                .id(a.getArticleId())
-                .category(c)
-                .title(d.getTitle())
-                .date(d.getPublishTime())
-                .url(a.getUrl())
-                .content(d.getContentCol())
-                .subtitle(d.getSubCol())
-                .summary(d.getGptResult().getSummary())
-                .keywords(d.getGptResult().getKeywords())
-                .quizzes(d.getGptResult().getQuizzes())
-                .build();
-    }
+    private List<GptDocument.GptResult.Keyword> keywords;
+    private List<GptDocument.GptResult.Quiz> quizzes;
 }
