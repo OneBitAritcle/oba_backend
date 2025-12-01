6ed8179ed839c26bef6ebb39d49d604fa872aa2f
Byunjihun <vkvlditm00@naver.com>
2025-12-02 00:15:22 +0900

    ON-79 IncorrectQuizRepository 추가 - 오답 퀴즈 저장 Repository 생성

--- server/src/main/java/oba/backend/server/domain/quiz/IncorrectQuizRepository.java
+++ server/src/main/java/oba/backend/server/domain/quiz/IncorrectQuizRepository.java
@@ -1,4 +1,11 @@
 package oba.backend.server.domain.quiz;
 
-public class IncorrectQuizRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface IncorrectQuizRepository extends JpaRepository<IncorrectQuiz, IncorrectQuizId> {
+
+    List<IncorrectQuiz> findByUserId(Long userId);
+
+    void deleteByUserIdAndArticleId(Long userId, Long articleId);
 }
