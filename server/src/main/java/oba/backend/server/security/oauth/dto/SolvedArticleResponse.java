ecec615b00da1063fc79bdfcd74c4bd24a0a899a
Byunjihun <vkvlditm00@naver.com>
2025-12-02 00:17:19 +0900

    ON-79 SolvedArticleResponse 수정 - DTO 구조 및 응답 데이터 보완

--- server/src/main/java/oba/backend/server/security/oauth/dto/SolvedArticleResponse.java
+++ server/src/main/java/oba/backend/server/security/oauth/dto/SolvedArticleResponse.java
@@ -1,4 +1,13 @@
-package oba.backend.server.security.oauth.dto;
+package oba.backend.server.domain.quiz.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class SolvedArticleResponse {
+    private Long articleId;
+    private LocalDateTime solvedAt;
 }
