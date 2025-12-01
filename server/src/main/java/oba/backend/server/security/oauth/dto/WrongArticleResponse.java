a29307574af1d3a33554e296fc06f29f52f9107f
Byunjihun <vkvlditm00@naver.com>
2025-12-02 00:17:22 +0900

    ON-79 WrongArticleResponse 수정 - 오답 응답 DTO 업데이트

--- server/src/main/java/oba/backend/server/security/oauth/dto/WrongArticleResponse.java
+++ server/src/main/java/oba/backend/server/security/oauth/dto/WrongArticleResponse.java
@@ -1,4 +1,11 @@
-package oba.backend.server.security.oauth.dto;
+package oba.backend.server.domain.quiz.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class WrongArticleResponse {
+    private Long articleId;
+    private boolean[] wrongList;   // ex: [false, true, false, true, false]
 }
