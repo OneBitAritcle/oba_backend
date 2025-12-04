07dae1f5f4a09c732df3935a589fdb6dc88dd8b1
Byunjihun <vkvlditm00@naver.com>
2025-12-04 15:46:18 +0900

    ON-79 BaseEntity MongoDB 전용 패키지로 이동 - RDB 구조 제거

--- server/src/main/java/oba/backend/server/entity/mongo/BaseEntity.java
+++ server/src/main/java/oba/backend/server/entity/mongo/BaseEntity.java
@@ -1,8 +1,7 @@
-package oba.backend.server.entity;
+package oba.backend.server.entity.mongo;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
