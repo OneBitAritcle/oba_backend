c2c5d4d240f13f41610a0a8cc763a2cc7bd07cfc
Byunjihun <vkvlditm00@naver.com>
2025-12-04 15:45:57 +0900

    ON-79 EnvVarPostProcessor 수정 - 환경변수 로딩 방식 개선

--- server/src/main/java/oba/backend/server/config/EnvVarPostProcessor.java
+++ server/src/main/java/oba/backend/server/config/EnvVarPostProcessor.java
@@ -1,4 +1,22 @@
 package oba.backend.server.config;
 
-public class EnvVarPostProcessor {
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class EnvVarPostProcessor implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment,
+                                       SpringApplication application) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            environment.getSystemProperties().put(entry.getKey(), entry.getValue());
+        });
+    }
 }
