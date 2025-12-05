89f36af6b496c1dc449d892b9bcdb6f9f232c8db
jihun <jihun@jihunui-MacBookPro.local>
2025-12-05 11:37:29 +0900

    ON-79 UserRepository 수정 - provider 기반 identifier 조회 메서드 정리

--- server/src/main/java/oba/backend/server/repository/user/UserRepository.java
+++ server/src/main/java/oba/backend/server/repository/user/UserRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByIdentifier(String identifier);
-    Optional<User> findByEmail(String email);
 }
