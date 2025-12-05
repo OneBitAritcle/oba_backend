fb5352c62ed5474ff23b27dc23feec92172da3c4
jihun <jihun@jihunui-MacBookPro.local>
2025-12-05 11:37:24 +0900

    ON-79 LoginRequest DTO 수정 - 모바일 로그인 전용 idToken 추가

--- server/src/main/java/oba/backend/server/dto/LoginRequest.java
+++ server/src/main/java/oba/backend/server/dto/LoginRequest.java
@@ -1,10 +1,8 @@
 package oba.backend.server.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class LoginRequest {
     private String idToken;
 }
