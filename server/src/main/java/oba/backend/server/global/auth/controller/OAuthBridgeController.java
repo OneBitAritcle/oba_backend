430863b6937f44e5a02ec3d808969975ce60e6a7
“Byunjihun” <vkvlditm00@naver.com>
2026-01-09 10:36:40 +0900

    ON-79 OAuthBridgeController 이동

--- server/src/main/java/oba/backend/server/global/auth/controller/OAuthBridgeController.java
+++ server/src/main/java/oba/backend/server/global/auth/controller/OAuthBridgeController.java
@@ -1,4 +1,4 @@
-package oba.backend.server.controller;
+package oba.backend.server.global.auth.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
