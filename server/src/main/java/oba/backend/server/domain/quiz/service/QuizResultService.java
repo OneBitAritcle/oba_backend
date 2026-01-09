0d437cf7d05ecbe4850774e19e06ce5b1c47e08c
“Byunjihun” <vkvlditm00@naver.com>
2026-01-09 10:38:02 +0900

    ON-79 QuizService 이동

--- server/src/main/java/oba/backend/server/domain/quiz/service/QuizResultService.java
+++ server/src/main/java/oba/backend/server/domain/quiz/service/QuizResultService.java
@@ -1,10 +1,10 @@
-package oba.backend.server.doma.quiz.service;
+package oba.backend.server.domain.quiz.service;
 
 import lombok.RequiredArgsConstructor;
 import oba.backend.server.global.auth.jwt.JwtProvider;
-import oba.backend.server.doma.quiz.dto.QuizResultRequest;
-import oba.backend.server.doma.quiz.entity.IncorrectArticles;
-import oba.backend.server.doma.quiz.repository.IncorrectArticlesRepository;
+import oba.backend.server.domain.quiz.dto.QuizResultRequest;
+import oba.backend.server.domain.quiz.entity.IncorrectArticles;
+import oba.backend.server.domain.quiz.repository.IncorrectArticlesRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
