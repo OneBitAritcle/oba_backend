0b88735208fef15c433528c14fdb58f0da2d43ce
“Byunjihun” <vkvlditm00@naver.com>
2026-01-09 10:38:02 +0900

    ON-79 QuizQueryService 이동

--- server/src/main/java/oba/backend/server/domain/quiz/service/QuizQueryService.java
+++ server/src/main/java/oba/backend/server/domain/quiz/service/QuizQueryService.java
@@ -1,10 +1,10 @@
-package oba.backend.server.doma.quiz.service;
+package oba.backend.server.domain.quiz.service;
 
 import lombok.RequiredArgsConstructor;
-import oba.backend.server.doma.quiz.dto.SolvedArticleResponse;
-import oba.backend.server.doma.quiz.dto.WrongArticleResponse;
-import oba.backend.server.doma.quiz.repository.IncorrectArticlesRepository;
-import oba.backend.server.doma.quiz.repository.IncorrectQuizRepository;
+import oba.backend.server.domain.quiz.dto.SolvedArticleResponse;
+import oba.backend.server.domain.quiz.dto.WrongArticleResponse;
+import oba.backend.server.domain.quiz.repository.IncorrectArticlesRepository;
+import oba.backend.server.domain.quiz.repository.IncorrectQuizRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
